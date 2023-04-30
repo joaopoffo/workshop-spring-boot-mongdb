1f91774a7c9f6d49997d219b0f420d49d765df88
Joao Poffo <j.poffo23@gmail.com>
2023-04-30 13:38:39 -0300

    Insercao de usuario com POST

--- src/main/java/com/joaopoffo/workshopmongo/services/UserService.java
+++ src/main/java/com/joaopoffo/workshopmongo/services/UserService.java
@@ -1,12 +1,12 @@
 package com.joaopoffo.workshopmongo.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaopoffo.workshopmongo.domain.User;
+import com.joaopoffo.workshopmongo.dto.UserDTO;
 import com.joaopoffo.workshopmongo.repository.UserRepository;
 import com.joaopoffo.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +26,13 @@ public class UserService {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return user;
+	}
+	
+	public User insert(User obj) {
+	return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
 }
